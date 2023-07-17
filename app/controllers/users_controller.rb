e8f02f3d2487f18a7b464d6ce1d5c7a524b4cd1f
Luis <lvas248@gmail.com>
2023-07-17 14:10:04 -0400

    create route and controller action for refresh functionality, backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,20 @@ class UsersController < ApplicationController
         render json: user, status: :created
     end
 
+    #refresh
+    def show
+        user = get_user
+        render json: user, status: :ok
+    end
+
     private
 
     def user_params
         params.permit(:username, :email, :password, :password_confirmation)
+    end
+
+    def get_user
+        User.find(session[:user_id])
     end
 
 end
