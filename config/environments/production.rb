50aec06be14ac16e1b5ee37a70a5fbdde82a91b8
Luis <lvas248@gmail.com>
2023-07-28 09:25:03 -0400

    update prodection environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,22 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            'lvas248.dev@gmail.com',
+    password:             'qmbnefcqyxkpsmqy',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 
+  }
+
 end
