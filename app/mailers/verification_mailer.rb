058f65ff5e753080468d55737d0f138a88584367
Luis <lvas248@gmail.com>
2023-09-09 18:04:52 -0400

    update email verification link address

--- app/mailers/verification_mailer.rb
+++ app/mailers/verification_mailer.rb
@@ -3,7 +3,7 @@ class VerificationMailer < ApplicationMailer
 
     def verify_email
         @verification = params[:verification]
-        @url = ENV['DOMAIN'] + 'email_verification/token/' + @verification.code
+        @url = ENV['DOMAIN'] + '/email_verification/token/' + @verification.code
         mail(to: @verification.user.email, subject: 'Welcome')
     end
 
